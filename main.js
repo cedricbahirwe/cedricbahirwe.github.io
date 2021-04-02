0a8ff186b04b00a23c14166f71b62b9680cfcf9a
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-04-02 21:13:29 +0200

    Add Scrolling Effect and Sticky animation

---
+++ main.js
@@ -0,0 +1,15 @@
+window.onscroll = function() { scrollUpdater() };
+
+// Get the navbar
+var navbar = document.getElementsByTagName("header")[0];
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function scrollUpdater() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
