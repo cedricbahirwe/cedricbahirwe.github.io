e8b1c571526d5169cb1acd2bd79259a459cf5c47
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-04-02 22:18:44 +0200

    Impove animation and fix bug on sticky bar

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function scrollUpdater() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
+    if (window.pageYOffset > sticky) {
+        navbar.classList.add("sticky");
     } else {
         navbar.classList.remove("sticky");
     }
