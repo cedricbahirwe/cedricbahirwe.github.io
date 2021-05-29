b19822080d206bd648f1e3d7f4e63fe3fa5d1e6c
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-05-29 23:00:26 +0200

    Improve Layout 1

--- new.js
+++ new.js
@@ -1,5 +1,13 @@
 
-let el = document.getElementsByTagName('li')[0]
+let el = document.getElementsByTagName('a')[0]
 
 el.classList.add('active')
+
+var inputs = document.querySelectorAll("a");                               
+for(var i = 0; i < inputs.length; i++){                                                                  
+   inputs[i].addEventListener("click", function(e){          
+        inputs.forEach(el => el.classList.remove('active'))
+        e.target.classList.add('active')                                                    
+    });                           
+ }
 
