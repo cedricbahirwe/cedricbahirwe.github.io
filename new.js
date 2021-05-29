2b5b7527d2984c018c6d8630f6682b91436876d1
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-05-30 01:30:18 +0200

    tab index managemenent

--- new.js
+++ new.js
@@ -1,13 +1,22 @@
 
+const navbar = document.getElementById('navbar')
+
 let el = document.getElementsByTagName('a')[0]
 
 el.classList.add('active')
 
 var inputs = document.querySelectorAll("a");                               
 for(var i = 0; i < inputs.length; i++){                                                                  
-   inputs[i].addEventListener("click", function(e){          
-        inputs.forEach(el => el.classList.remove('active'))
-        e.target.classList.add('active')                                                    
+   inputs[i].addEventListener("click", function(e){   
+       if (navbar.contains(e.target)) {
+        inputs.forEach((el) =>  {
+            el.classList.remove('active')
+        })
+        e.target.classList.add('active')   
+       } else {
+           console.log("Don't add activty")
+       }
+                                                         
     });                           
  }
 
