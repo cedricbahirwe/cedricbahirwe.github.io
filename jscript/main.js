af95b17a470e8e0fcbc36038c370aebe6402e4ed
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-06-14 01:03:35 +0200

    Add Dark Mode v1.1

--- jscript/main.js
+++ jscript/main.js
@@ -2,21 +2,28 @@
 
 
 // An agnostic support for dark mode
-const themeCheckBox = document.getElementById('theme')
+const cedriSiteThemeId = "cedric.site.theme"
+const themeCheckBox = document.getElementById('theme');
 themeCheckBox.addEventListener("click", changeTheme);
 const rootDataset = document.documentElement.dataset;
+// Check browser support
+if (typeof(Storage) !== "undefined") {
+    // Retrieve Theme
+    rootDataset.theme =  localStorage.getItem(cedriSiteThemeId)
+  } else {
+    console.log("Sorry, your browser does not support Web Storage...");
+}
 function changeTheme(theme) {
     const inDarkMode = (rootDataset.theme === 'dark');
     rootDataset.theme = inDarkMode ? '' : 'dark';
+    // Store Theme
+    localStorage.setItem(cedriSiteThemeId, rootDataset.theme);
 }
 
 // The goal is to get the first the clicked link in the navigation section and style it,
 // By default (Home page) we're setting and styling the first anchor tag found.
 // Need better approach.
 const navbar = document.getElementById('navbar')
-// let firstAnchor = document.getElementsByTagName('a')[0]
-// firstAnchor.classList.add('active')
-
 const inputs = navbar.querySelectorAll("a");                               
 for(var i = 0; i < inputs.length-1; i++){                                                                  
    inputs[i].addEventListener("click", styleAnchor);
