9076b0cf3c72b8952101bab3d695d6928a1bde39
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-06-29 17:49:13 +0200

    Remove dark mode

--- jscript/main.js
+++ jscript/main.js
@@ -1,23 +1,23 @@
 // I'm more a JS thinker than writer
 
 // An agnostic support for dark mode
-const cedricSiteThemeId = "cedric.site.theme"
-const themeCheckBox = document.getElementById('theme');
-themeCheckBox.addEventListener("click", changeTheme);
-const rootDataset = document.documentElement.dataset;
-// Check browser support
-if (typeof(Storage) !== "undefined") {
-    // Retrieve Theme
-    rootDataset.theme =  localStorage.getItem(cedricSiteThemeId)
-  } else {
-    console.log("Sorry, your browser does not support Web Storage...");
-}
-function changeTheme() {
-    const inDarkMode = (rootDataset.theme === 'dark');
-    rootDataset.theme = inDarkMode ? '' : 'dark';
-    // Store Theme
-    localStorage.setItem(cedricSiteThemeId, rootDataset.theme);
-}
+// const cedricSiteThemeId = "cedric.site.theme"
+// const themeCheckBox = document.getElementById('theme');
+// themeCheckBox.addEventListener("click", changeTheme);
+// const rootDataset = document.documentElement.dataset;
+// // Check browser support
+// if (typeof(Storage) !== "undefined") {
+//     // Retrieve Theme
+//     rootDataset.theme =  localStorage.getItem(cedricSiteThemeId)
+//   } else {
+//     console.log("Sorry, your browser does not support Web Storage...");
+// }
+// function changeTheme() {
+//     const inDarkMode = (rootDataset.theme === 'dark');
+//     rootDataset.theme = inDarkMode ? '' : 'dark';
+//     // Store Theme
+//     localStorage.setItem(cedricSiteThemeId, rootDataset.theme);
+// }
 
 // The goal is to get the first the clicked link in the navigation section and style it,
 // By default (Home page) we're setting and styling the first anchor tag found.
