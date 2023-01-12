363b90b693153bea8393bc40056e0359a8cabffe
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2023-01-13 01:24:14 +0200

    i'm loosing it

--- jscript/main.js
+++ jscript/main.js
@@ -1,39 +1,86 @@
 // I'm more a JS thinker than writer
 
 // An agnostic support for dark mode
-// const cedricSiteThemeId = "cedric.site.theme"
-// const themeCheckBox = document.getElementById('theme');
-// themeCheckBox.addEventListener("click", changeTheme);
-// const rootDataset = document.documentElement.dataset;
-// // Check browser support
-// if (typeof(Storage) !== "undefined") {
-//     // Retrieve Theme
-//     rootDataset.theme =  localStorage.getItem(cedricSiteThemeId)
-//   } else {
-//     console.log("Sorry, your browser does not support Web Storage...");
-// }
-// function changeTheme() {
-//     const inDarkMode = (rootDataset.theme === 'dark');
-//     rootDataset.theme = inDarkMode ? '' : 'dark';
-//     // Store Theme
-//     localStorage.setItem(cedricSiteThemeId, rootDataset.theme);
-// }
+const cedricSiteThemeId = "cedric.site.theme"
+const rootDataset = document.documentElement.dataset;
+const lightMode = 'light';
+const darkMode = 'dark';
+
+// Check browser support and default to our theme
+// First time page appear
+window.onload = function () {
+    if (typeof (Storage) !== "undefined") {
+        // Retrieve Stored Theme
+        const storedTheme = localStorage.getItem(cedricSiteThemeId);
+
+        if (storedTheme == null) {
+            const isLightMode = isCurrentThemeLight();
+            rootDataset.theme = isLightMode ? lightMode : darkMode;
+        } else {
+            rootDataset.theme = storedTheme;
+        }
+
+        if (rootDataset.theme === lightMode) {
+            darkModeSwitch.style.justifyContent = 'flex-start';
+        } else {
+            darkModeSwitch.style.justifyContent = 'flex-end';
+        }
+        localStorage.setItem(cedricSiteThemeId, rootDataset.theme);
+    } else {
+        console.log("Sorry, your browser does not support Web Storage...");
+    }
+}
+
+
+// Get the current device theme
+function isCurrentThemeLight() {
+    const isPrefersLight = window.matchMedia('(prefers-color-scheme: light)').matches;
+    return isPrefersLight
+}
+
+const darkModeSwitch = document.getElementById('darkmode-toggle');
+darkModeSwitch.addEventListener("click", toggleTheme);
+
+// Toggle between light and dark mode
+function toggleTheme(e) {
+    document.documentElement.classList.add('transition');
+
+    console.log(e.target.checked);
+    const storedTheme = localStorage.getItem(cedricSiteThemeId);
+    let nextTheme;
+    if (storedTheme == null) {
+        const isLightMode = isCurrentThemeLight();
+        nextTheme = isLightMode ? lightMode : darkMode;
+    } else {
+        nextTheme = storedTheme == lightMode ? darkMode : lightMode;
+    }
+    rootDataset.theme = nextTheme;
+    localStorage.setItem(cedricSiteThemeId, rootDataset.theme);
+    
+    console.log("sss", rootDataset.theme );
+    if (rootDataset.theme === darkMode) {
+        darkModeSwitch.style.justifyContent = 'flex-end';
+    } else {
+        darkModeSwitch.style.justifyContent = 'flex-start';
+    }
+    darkModeSwitch.classList.toggle('is-dark-mode')
+}
 
 // The goal is to get the first the clicked link in the navigation section and style it,
 // By default (Home page) we're setting and styling the first anchor tag found.
 // Need better approach.
 const navbar = document.getElementById('navbar')
-const inputs = navbar.querySelectorAll("a");                               
-for(let i = 0; i < inputs.length-1; i++){
-   inputs[i].addEventListener("click", styleAnchor);
+const inputs = navbar.querySelectorAll("a");
+for (let i = 0; i < inputs.length - 1; i++) {
+    inputs[i].addEventListener("click", styleAnchor);
 }
 
 function styleAnchor(e) {
     if (navbar.contains(e.target)) {
-        inputs.forEach((el) =>  {
+        inputs.forEach((el) => {
             el.classList.remove('active')
         })
-        e.target.classList.add('active')   
+        e.target.classList.add('active')
     } else {
         console.log("Don't add activity")
     }
