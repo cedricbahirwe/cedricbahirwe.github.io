267763d87b014a1133bf0f8fc780934e136fbf07
Cédric Bahirwe <cedricaganzebahirwe@gmail.com>
2025-04-25 19:00:39 +0300

    Improve rss

--- app/rss/route.ts
+++ app/rss/route.ts
@@ -1,42 +1,45 @@
-import { baseUrl } from 'app/sitemap'
-import { getBlogPosts } from 'app/blog/utils'
+import { baseUrl } from "app/sitemap";
+import { getBlogPosts } from "app/blog/utils";
 
 export async function GET() {
-  let allBlogs = await getBlogPosts()
+	let allBlogs = await getBlogPosts();
 
-  const itemsXml = allBlogs
-    .sort((a, b) => {
-      if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
-        return -1
-      }
-      return 1
-    })
-    .map(
-      (post) =>
-        `<item>
+	const itemsXml = allBlogs
+		.sort((a, b) => {
+			if (
+				new Date(a.metadata.publishedAt) >
+				new Date(b.metadata.publishedAt)
+			) {
+				return -1;
+			}
+			return 1;
+		})
+		.map(
+			(post) =>
+				`<item>
           <title>${post.metadata.title}</title>
           <link>${baseUrl}/blog/${post.slug}</link>
-          <description>${post.metadata.summary || ''}</description>
+          <description>${post.metadata.summary || ""}</description>
           <pubDate>${new Date(
-            post.metadata.publishedAt
-          ).toUTCString()}</pubDate>
+				post.metadata.publishedAt
+			).toUTCString()}</pubDate>
         </item>`
-    )
-    .join('\n')
+		)
+		.join("\n");
 
-  const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
+	const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
   <rss version="2.0">
     <channel>
-        <title>My Portfolio</title>
+        <title>Cedric's Portfolio</title>
         <link>${baseUrl}</link>
         <description>This is my portfolio RSS feed</description>
         ${itemsXml}
     </channel>
-  </rss>`
+  </rss>`;
 
-  return new Response(rssFeed, {
-    headers: {
-      'Content-Type': 'text/xml',
-    },
-  })
+	return new Response(rssFeed, {
+		headers: {
+			"Content-Type": "text/xml",
+		},
+	});
 }
