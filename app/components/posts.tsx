bc7538051c3747a81879c74a05c8207cef178c15
Cédric Bahirwe <cedricaganzebahirwe@gmail.com>
2026-05-20 10:51:31 +0200

    Support external posts in blog feed

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -1,36 +1,30 @@
 import Link from 'next/link'
-import { formatShortDate, getBlogPosts } from 'app/blog/utils'
+import { getAllBlogPosts, type BlogPost } from 'app/blog/posts'
+import { formatShortDate } from 'app/blog/utils'
+
+function PostLink({ post }: { post: BlogPost }) {
+  return (
+    <Link className="flex flex-col space-y-1 mb-4" href={post.href}>
+      <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+        <p className="text-neutral-600 dark:text-neutral-400 min-w-[100px] tabular-nums">
+          {formatShortDate(post.publishedAt)}
+        </p>
+        <p className="text-neutral-900 dark:text-neutral-100 tracking-tight underline underline-offset-2 transition-colors hover:text-red-700 dark:hover:text-red-400">
+          {post.title}
+        </p>
+      </div>
+    </Link>
+  )
+}
 
 export function BlogPosts() {
-  let allBlogs = getBlogPosts()
+  let allBlogs = getAllBlogPosts()
 
   return (
     <div>
-      {allBlogs
-        .sort((a, b) => {
-          if (
-            new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
-          ) {
-            return -1
-          }
-          return 1
-        })
-        .map((post) => (
-          <Link
-            key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
-            href={`/blog/${post.slug}`}
-          >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 min-w-[100px] tabular-nums">
-                {formatShortDate(post.metadata.publishedAt)}
-              </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                {post.metadata.title}
-              </p>
-            </div>
-          </Link>
-        ))}
+      {allBlogs.map((post) => (
+        <PostLink key={post.href} post={post} />
+      ))}
     </div>
   )
 }
